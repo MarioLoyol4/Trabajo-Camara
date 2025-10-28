5290e92cdc8497586383d6bd0666c2cc8a9e1aa5
MarioLoyol4 <mar.loyolac@duocuc.cl>
2025-10-28 20:02:54 -0300

    Funcionalidad cámara y galería integrada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.trabajo_camara"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.trabajo_camara"
